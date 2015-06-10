bc350f891ab17889d682566fd051975af6c3b21e
erimendes <eri.mendes32@gmail.com>
2015-06-10 07:56:57 -0300

    Update MainActivity.java

--- MeuCelular2/src/com/example/meucelular2/MainActivity.java
+++ MeuCelular2/src/com/example/meucelular2/MainActivity.java
@@ -11,51 +11,95 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 
-public class MainActivity extends Activity {
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
-	}
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
-	}
+public class MainActivity extends Activity  {
+   Button b1,b2;
+   EditText ed1,ed2;
+   
+   TextView tx1;
+   int counter = 3;
+   
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+      
+      b1=(Button)findViewById(R.id.button);
+      ed1=(EditText)findViewById(R.id.editText);
+      ed2=(EditText)findViewById(R.id.editText2);
+      
+      b2=(Button)findViewById(R.id.button2);
+      tx1=(TextView)findViewById(R.id.textView3);
+      tx1.setVisibility(View.GONE);
+      
+      b1.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+            if(ed1.getText().toString().equals("admin") &&
+         
+            ed2.getText().toString().equals("admin")) {
+               Toast.makeText(getApplicationContext(), "Redirecting...",Toast.LENGTH_SHORT).show();
+            }
+            else{
+               Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
+               
+               tx1.setVisibility(View.VISIBLE);
+               tx1.setBackgroundColor(Color.RED);
+               counter--;
+               tx1.setText(Integer.toString(counter));
+            
+               if (counter == 0) {
+                  b1.setEnabled(false);
+               }
+            }
+         }
+      });
+      
+      b2.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+            finish();
+         }
+      });
+   }
+   
+   @Override
+   public boolean onCreateOptionsMenu(Menu menu) {
+      // Inflate the menu; this adds items to the action bar if it is present.
+      getMenuInflater().inflate(R.menu.menu_main, menu);
+      return true;
+   }
+   
+   @Override
+   public boolean onOptionsItemSelected(MenuItem item) {
+      // Handle action bar item clicks here. The action bar will
+      // automatically handle clicks on the Home/Up button, so long
+      // as you specify a parent activity in AndroidManifest.xml.
+      
+      int id = item.getItemId();
+      
+      //noinspection SimplifiableIfStatement
+      if (id == R.id.action_settings) {
+         return true;
+      }
+      return super.onOptionsItemSelected(item);
+   }
 }
